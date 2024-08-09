135b92a283b740f438065cfa83cdb7e44a5ada34
Marc Frances <marc.frances.t@gmail.com>
2024-08-09 13:13:20 +0200

    Activity 9.10 done

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 
+import diagnoseRouter from './routes/diagnoses';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -9,6 +11,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
