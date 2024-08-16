28342136e634366503973f0c1b2bf5e8aacad7e5
Marc Frances <marc.frances.t@gmail.com>
2024-08-16 13:13:07 +0200

    Activity 9.15 done

--- course-info/src/App.tsx
+++ course-info/src/App.tsx
@@ -1,45 +1,87 @@
-const Header = ({ name }: { name: string }) => <h1>{name}</h1>
+import courseParts, { CoursePart } from './data';
 
-interface CoursePart {
-  name: string
-  exerciseCount: number
-}
+const Header = ({ name }: { name: string }) => <h1>{name}</h1>;
+
+const Part = ({ part }: { part: CoursePart }) => {
+  const noBottomMargin = { marginBottom: 0 };
+  const noTopMargin = { marginTop: 0 };
+
+  const partTitle = (
+    <h3 style={noBottomMargin}>
+      {part.name} {part.exerciseCount}
+    </h3>
+  );
+
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`,
+    );
+  };
+
+  switch (part.kind) {
+    case 'basic':
+      return (
+        <div>
+          {partTitle}
+          <p style={noTopMargin}>
+            <em>{part.description}</em>
+          </p>
+        </div>
+      );
+
+    case 'group':
+      return (
+        <div>
+          {partTitle}
+          <p style={noTopMargin}>Project exercises: {part.groupProjectCount}</p>
+        </div>
+      );
+
+    case 'background':
+      return (
+        <div>
+          {partTitle}
+          <p style={{ ...noTopMargin, ...noBottomMargin }}>
+            <em>{part.description}</em>
+          </p>
+          <p style={noTopMargin}>Submit to: {part.backgroundMaterial}</p>
+        </div>
+      );
+
+    case 'special':
+      return (
+        <div>
+          {partTitle}
+          <p style={{ ...noTopMargin, ...noBottomMargin }}>
+            <em>{part.description}</em>
+          </p>
+          <p style={noTopMargin}>
+            Required skills: {part.requirements.join(', ')}
+          </p>
+        </div>
+      );
+
+    default:
+      return assertNever(part);
+  }
+};
 
 const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
   return courseParts.map((part, index) => {
-    return (
-      <p key={index}>
-        {part.name} {part.exerciseCount}
-      </p>
-    )
-  })
-}
+    return <Part key={index} part={part} />;
+  });
+};
 
 const Footer = ({ total }: { total: number }) => (
   <p>Number of exercises {total}</p>
-)
+);
 
 const App = () => {
-  const courseName = 'Half Stack application development'
-  const courseParts = [
-    {
-      name: 'Fundamentals',
-      exerciseCount: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exerciseCount: 7,
-    },
-    {
-      name: 'Deeper type usage',
-      exerciseCount: 14,
-    },
-  ]
-
+  const courseName = 'Half Stack application development';
   const totalExercises = courseParts.reduce(
     (sum, part) => sum + part.exerciseCount,
-    0
-  )
+    0,
+  );
 
   return (
     <>
@@ -47,7 +89,7 @@ const App = () => {
       <Content courseParts={courseParts} />
       <Footer total={totalExercises} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
