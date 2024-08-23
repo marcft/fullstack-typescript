61f408b7c392c8d2da75bb2ed26e05c4c3c03eab
Marc Frances <marc.frances.t@gmail.com>
2024-08-23 16:14:03 +0200

    Activity 9.24 done

--- patientor/frontend/src/components/PatientDetailsPage/PatientEntries.tsx
+++ patientor/frontend/src/components/PatientDetailsPage/PatientEntries.tsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from 'react';
 import { Typography } from '@mui/material';
 
-import { Entry } from '../../types';
+import { Diagnosis, Entry } from '../../types';
+import diagnosesService from '../../services/diagnoses';
 
 interface Props {
   entries: Entry[];
 }
 
 const PatientEntries = ({ entries }: Props) => {
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      const patient = await diagnosesService.getAll();
+      setDiagnoses(patient);
+    };
+    void fetchPatient();
+  }, []);
+
+  const getDiagnosisForCode = (code: Diagnosis['code']) => {
+    const diagnosis = diagnoses.find((d) => d.code === code);
+    if (diagnosis) return diagnosis.name;
+
+    return 'No diagnosis found with this code';
+  };
+
   return (
     <>
       <Typography variant="h5" style={{ margin: '0.5em 0' }}>
@@ -21,7 +40,9 @@ const PatientEntries = ({ entries }: Props) => {
             <ul>
               {entry.diagnosisCodes?.map((code) => (
                 <li key={code}>
-                  <Typography variant="body2">{code}</Typography>
+                  <Typography variant="body2">
+                    {code}: {getDiagnosisForCode(code)}
+                  </Typography>
                 </li>
               ))}
             </ul>
